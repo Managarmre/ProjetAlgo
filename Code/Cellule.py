ac6be69d189e212f83fae5af453e175d21e270ef
Maxime Pineau <maxime.nicolas2@hotmail.fr>
2015-01-25 14:53:56 +0000

    modif code et ajout doc

--- Code/Cellule.py
+++ Code/Cellule.py
@@ -1,28 +1,39 @@
 
 import Lien as li
+from Exceptions import CelluleException
 
 
-# une cellule du jeu
-# représente un sommet d'un graphe
+
+
 class Cellule:
+	"""
+	Une cellule du jeu. Elle peut être représentée par un sommet dans un graphe.
+	"""
 
-	"""
-		Int numero : le numéro identifiant la cellule (unique pour chaque cellule)
-		Int attaque : le nombre d'unités attaquantes sur la cellule actuellement
-		Int defense : le nombre d'unités defensives sur la cellule actuellement
-		Int attaqueMax : le nombre d'unités attaquantes maximal sur la cellule
-		Int defenseMax : le nombre d'unités defensives maximal sur la cellule 
-		Int production : vitesse de production des unités de la cellule
-		Int couleurJoueur: couleur du joueur à qui appartient la cellule (-1 si neutre)
-		Int x : la coordonnée x de la cellule sur le terrain (graphique)
-		Int y : la coordonnée y de la cellule sur le terrain (graphique)
-		Int rayon : le rayon de la cellule (graphique)
-	"""
-	def __init__(self, numero, attaque, defense, attaqueMax, defenseMax, production, couleurJoueur, x, y, rayon):
-		
+	def __init__(self, numero, attaque, defense, attaqueMax, defenseMax, production, couleur, x, y, rayon):
+		"""
+		Constructeur de la classe Cellule
+
+		:param int numero: le numéro identifiant la cellule (unique pour chaque cellule)
+		:param int attaque: le nombre d'unités attaquantes sur la cellule actuellement
+		:param int defense: le nombre d'unités defensives sur la cellule actuellement
+		:param int attaqueMax: le nombre d'unités attaquantes maximal sur la cellule
+		:param int defenseMax: le nombre d'unités defensives maximal sur la cellule
+		:param in production: vitesse de production des unités attaquantes de la cellule
+		:param int couleur: couleur du joueur à qui appartient la cellule (-1 si neutre)
+		:param int x: la coordonnée x de la cellule sur le terrain (graphique)
+		:param int y: la coordonnée y de la cellule sur le terrain (graphique)
+		:param int rayon: le rayon de la cellule (graphique)
+
+		:returns: une cellule 
+		:rtype: class'Cellule'
+		:raises :class:'CelluleException': si l'attaque est inférieur à 0 ou supérieur à l'attaque maximale
+		:raises :class:'CelluleException': si la défense est inférieur à 0 ou supérieur à la défense maximale
+		"""
+
 		# on vérifie les paramètres entrés pour la création de la cellule
-		noms = [ "numero", "attaque", "defense", "attaqueMax", "defenseMax", "production", "couleurJoueur", "x", "y", "rayon" ]
-		valeurs = [ numero, attaque, defense, attaqueMax, defenseMax, production, couleurJoueur, x, y, rayon ]
+		noms = [ "numero", "attaque", "defense", "attaqueMax", "defenseMax", "production", "couleur", "x", "y", "rayon" ]
+		valeurs = [ numero, attaque, defense, attaqueMax, defenseMax, production, couleur, x, y, rayon ]
 		
 		params = { 
 				"numero" : numero, 
@@ -31,24 +42,24 @@ class Cellule:
 				"attaqueMax": attaqueMax, 
 				"defenseMax": defenseMax, 
 				"production": production, 
-				"couleurJoueur": couleurJoueur, 
+				"couleur": couleur, 
 				"x": x, 
 				"y": y 
 		}
 		
 		for nom_param, valeur_param in params.items():
 			if( not isinstance( valeur_param , int ) ):
-				raise Exception("le paramètre '" + nom_param + "' doit être un entier")
+				raise CelluleException("le paramètre '" + nom_param + "' doit être un entier")
 			
-			if( nom_param not in [ "x", "y", "couleurJoueur" ] and valeur_param < 0 ):
-				raise Exception("le paramètre '" + nom_param + "' ne peut pas être inférieur à 0")
+			if( nom_param not in [ "x", "y", "couleur" ] and valeur_param < 0 ):
+				raise CelluleException("le paramètre '" + nom_param + "' ne peut pas être inférieur à 0")
 		
 		
 		if( attaque > attaqueMax ):
-			raise Exception("l'attaque de la cellule ne peut pas être supérieure à l'attaque maximale de la cellule")
+			raise CelluleException("l'attaque de la cellule ne peut pas être supérieure à l'attaque maximale de la cellule")
 			
 		if( defense > defenseMax ):
-			raise Exception("la defense de la cellule ne peut pas être supérieure à la defense maximale de la cellule")
+			raise CelluleException("la defense de la cellule ne peut pas être supérieure à la defense maximale de la cellule")
 
 
 
@@ -60,7 +71,7 @@ class Cellule:
 		self.defenseMax = defenseMax
 		self.production = production
 		
-		self.couleurJoueur = couleurJoueur
+		self.couleur = couleur
 
 		# attributs 'graphiques' de la cellule
 		self.x = x
@@ -72,61 +83,154 @@ class Cellule:
 
 	
 	def getNumero(self):
+		"""
+		Retourne le numéro unique identifiant la cellule
+		
+		:returns: le numéro de la cellule
+		:rtype: int
+		"""
 		return self.numero
 
 	def getAttaque(self):
+		"""
+		Retourne l'attaque actuelle de la cellule.
+		
+		:returns: l'attaque de la cellule
+		:rtype: int
+		"""
 		return self.attaque 
 
 	def getDefense(self):
+		"""
+		Retourne la défense actuelle de la cellule.
+		
+		:returns: la défense de la cellule
+		:rtype: int
+		"""
 		return self.defense 
 
+
 	def getProduction(self):
+		"""
+		Retourne la valeur de la cadence de production de la cellule
+		
+		:returns: la cadence de production de la cellule
+		:rtype: int
+		"""
 		return self.production
 
-	def getCouleurJoueur(self):
-		return self.couleurJoueur 
+
+	def getCouleur(self):
+		"""
+		Retourne la couleur de la cellule. Cette couleur correspond à un numéro de joueur supérieur ou égale à -1 (-1 étant la couleur du neutre).
+		Cette couleur permet de définir à quel joueur appartient la cellule.
+
+		:returns: la couleur de la cellule
+		:rtype: int
+		"""
+		return self.couleur 
+
 
 	def getAttaqueMax(self):
+		"""
+		Retourne la quantité maximale d'unités offensives que la cellule peut contenir
+		
+		:returns: l'attaque maximale de la cellule
+		:rtype: int
+		"""
 		return self.attaqueMax 
 
+
 	def getDefenseMax(self):
+		"""
+		Retourne la quantité maximale d'unités défensives que la cellule peut contenir
+		
+		:returns: la défense maximale de la cellule
+		:rtype: int
+		"""
 		return self.defenseMax 
 
+
 	def getLiens(self):
+		"""
+		Retourne les liens dont cette cellule est lune des extrémités.
+		
+		:returns: la liste des liens 
+		:rtype: liste de :class:'Lien'
+        """
 		return self.liens
 
 	
-	# retourne la liste de cellules voisines de celle-ci
-	# ATTENTION, peut renvoyer un résultat différent de la méthode Terrain.getVoisinsCellule(cellule)
 	def getVoisins(self):
+		"""
+		Retourne la liste des cellules voisines à celle-ci.
+
+		ATTENTION, peut renvoyer un résultat différent de la méthode Terrain.getVoisinsCellule( cellule )
+		car on ne modifiera jamais la liste des liens de la cellules, 
+		contrairement au terrain, ou l'on pourra ne considérer qu'une partie du terrain (un sous graphe), et donc qu'une partie des liens.
+		
+		:returns: la liste des cellules voisines 
+		:rtype: liste de :class:'Cellule'
+		"""
 		# comme chaque cellule n'est relié qu'au plus 1 seule fois à chaques cellules, pas besoin de vérifier les doublons ici
 		return [ lien.getOtherCellule(self) for lien in self.getLiens() ]
 	
 	
 	# retourne la liste des voisins ennemis
 	def getVoisinsEnnemis( self ):
-		return [ lien.getOtherCellule(self) for lien in self.getLiens() if lien.getOtherCellule(self).getCouleurJoueur() != self.getCouleurJoueur() ]
+		"""
+		Retourne la liste des cellules ennemies de cette cellule, c'est à dire celle qui n'ont pas la même couleur que celle de cette cellule.
+
+		:returns: la liste des cellules ennemies de cette cellule 
+		:rtype: liste de :class:'Cellule'
+		"""
+		return [ lien.getOtherCellule(self) for lien in self.getLiens() if not lien.getOtherCellule(self).aPourCouleur( self.getCouleur() ) ]
+	
+
+	def getVoisinsAllies( self ):
+		"""
+		Retourne la liste des cellules alliés de cette cellule, c'est à dire celle qui ont la même couleur que celle de cette cellule.
+
+		:returns: la liste des cellules alliés de cette cellule 
+		:rtype: liste de :class:'Cellule'
+		"""
+		return [ lien.getOtherCellule(self) for lien in self.getLiens() if lien.getOtherCellule(self).aPourCouleur( self.getCouleur() ) ]
 	
 	
-	
-	# non fini !!!!!!
-	#
-	#
-	#
-	# retourne le coût de la cellule, c'est à dire le nombre d'unités nécéssaire pour la capturer
 	def getCout(self):
+		"""
+		Retourne le coût de base de la cellule pour être conquise, c'est à dire le nombre d'unités nécéssaire pour la capturée ( attaque + défense ).
+		On ne prends pas en compte les unités présentes sur les liens.
+		
+		:returns: le coût de la cellule
+		:rtype: int
+		"""
 		return self.getAttaque() + self.getDefense() 
 	
 	
 	def getPourcentageAttaque( self ):
+		"""
+		Retourne à combien de pourcentage la cellule est pleine.
+		1 si l'attaque de la cellule est égale à l'attaque maximale.
+		
+		:returns: le pourcentage de l'attaque de la cellule
+		:rtype: int
+		"""
 		return self.getAttaque() / self.getAttaqueMax()
 	
 	
-	#
 	# ==> a voir avec la production !!!
-	#
 	def getExcedent( self ):
-		somme = sum( [ mouvement.getNbUnites() for lien in self.getLiens() for mouvement in lien.getMouvementVersCellule(self) if mouvement.getCouleurJoueur() == self.getCouleurJoueur() and mouvement.getTempsRestant() < 1000 ] )
+		"""
+		Retourne l'excédent de la cellule. 
+		L'excédent correspond au trop plein d'unités que la cellule va produire ou recevoir, et perdre (car elle aura déjà atteint son attaque maximale).
+		Seules les unités des mouvements arrivant vers cette cellules avec un temps restant inférieur à 1000 sont prises en comptes.
+
+		:returns: le nombre d'unités excédent
+		:rtype: int
+		"""
+
+		somme = sum( [ mouvement.getNbUnites() for mouvement in self.getMouvementsVersCellule() if mouvement.aPourCouleur( self.getCouleur() ) and mouvement.getTempsRestant() < 1000 ] )
 		
 		cellules_produites = self.getProduction()	
 		
@@ -141,86 +245,129 @@ class Cellule:
 			
 		else:
 			return 0
-		
-	
-	
+
 
 	def setAttaque(self, attaque):
+		"""
+		Affecte la valeur de la variable attaque actuelle.
+		La nouvelle valeur de l'attaque ne peut pas être inférieur à 0, ni excéder l'attaque maximale de la cellule.
 		
+		:param int attaque: la nouvelle valeur de l'attaque actuelle de la cellule 
+		:raises CelluleException: si l'attaque entrée en paramètre est inférieure à 0 ou supérieur à l'attaque maximale
+		"""
+
 		if( not isinstance( attaque , int ) ):
-			raise Exception("la valeur entrée n'est pas un entier")
+			raise CelluleException("la valeur entrée n'est pas un entier")
 			
 		elif( self.getAttaqueMax() < attaque ):
-			raise Exception("la valeur de l'attaque donnée (" , str(attaque) ,") est trop grande pour cette cellule (setAttaque), elle doit être inférieure à " + str(self.getAttaqueMax()) ) 
+			raise CelluleException("la valeur de l'attaque donnée (" , str(attaque) ,") est trop grande pour cette cellule (setAttaque), elle doit être inférieure à " + str(self.getAttaqueMax()) ) 
 			
 		elif( attaque < 0 ):
-			raise Exception("la valeur entrée doit être supérieure ou égale à zéro")
+			raise CelluleException("la valeur entrée doit être supérieure ou égale à zéro")
 			
 		self.attaque = attaque
 		
 
 	def setDefense(self, defense):
+		"""
+		Affecte la valeur de la variable defense actuelle.
+		La nouvelle valeur de la défense ne peut pas être inférieur à 0, ni excéder la défense maximale de la cellule.
 		
+		:param int defense: la nouvelle valeur de la défense actuelle de la cellule 
+		:raises CelluleException: si la défense entrée en paramètre est inférieure à 0 ou supérieur à la défense maximale
+		"""
+
 		if( not isinstance( defense , int ) ):
-			raise Exception("la valeur entrée n'est pas un entier")
+			raise CelluleException("la valeur entrée n'est pas un entier")
 			
 		elif( self.getDefenseMax() < defense ):
-			raise Exception("la valeur de la defense donnée (" , str(defense) ,") est trop grande pour cette cellule (setDefense), elle doit être inférieure à " + str( self.getDefenseMax() ) )
+			raise CelluleException("la valeur de la defense donnée (" , str(defense) ,") est trop grande pour cette cellule (setDefense), elle doit être inférieure à " + str( self.getDefenseMax() ) )
 			
 		elif( defense < 0 ):
-			raise Exception("la valeur entrée doit être suppérieure ou égale à 0")
+			raise CelluleException("la valeur entrée doit être suppérieure ou égale à 0")
 			
 		self.defense = defense 
 		
 
-	def setCouleurJoueur(self, couleurJoueur):
+	def setCouleur(self, couleur):
+		"""
+		Affecte la valeur de la variable couleur (la cellule change de propriétaire). 
+		Cette couleur doit être supérieur ou égale à -1, -1 étant la couleur du joueur neutre.
 		
-		if( not isinstance( couleurJoueur, int ) ):
-			raise Excpetion("la couleur d'un joueur doit être un entier")
+		:param int couleur: la nouvelle couleur de la cellule.
+		:raises :class:'CelluleException': si la couleur n'est pas supérieur ou égale à -1. 
+		"""
+
+		if( not isinstance( couleur, int ) ):
+			raise CelluleException("la couleur d'un joueur doit être un entier")
 			
-		elif( couleurJoueur < -1 ):
-			raise Exception("la couleur d'un joueur doit être suppérieure ou égale à -1")
+		elif( couleur < -1 ):
+			raise CelluleException("la couleur d'un joueur doit être suppérieure ou égale à -1")
 			
-		self.couleurJoueur = couleurJoueur
+		self.couleur = couleur
 
 
-
-
-
-	# ajoute un lien reliant cette cellule à une autre
-	# Lien lien : le lien à ajouté
 	def ajouterLien(self, lien):
+		"""
+		Ajoute un lien reliant cette cellule à une autre. 
 		
+		:param :class:'Lien' lien: le lien à ajouter
+		:raises :class:'CelluleException': si cette cellule n'est pas l'une des cellules aux extrémités du lien.
+		"""
+
 		if( not isinstance( lien, li.Lien ) ):
-			raise Exception("la valeur entrée n'est pas une instance de l'objet Lien")
+			raise CelluleException("la valeur entrée n'est pas une instance de l'objet Lien")
 		
-		if( lien.getU() != self and lien.getV() != self ):
-			raise Exception( "cette cellule n'est pas dans ce lien (ajouterLien)" )
+		if( not lien.celluleAppartientAuLien( self ) ):
+			raise CelluleException( "cette cellule n'est pas dans ce lien (ajouterLien)" )
 			
 		self.liens.append(lien)
 
 	
-	# retourne vrai si la cellule a pour couleur celle donnée ( appartient au joueur ayant cette couleur)
-	def aPourCouleur( self, couleurJoueur ):
-		return self.couleurJoueur == couleurJoueur
+	def aPourCouleur( self, couleur ):
+		"""
+		Retourne vrai si la cellule possède la couleur passée en paramètre, et faux sinon.
+		Autrement dit, elle retourne vrai si la cellule appartient au joueur ayant la couleur passée en paramètre.
+
+		:param int couleur: la couleur à vérifier 
+
+		:returns: retourne vrai si la cellule a bien pour couleur la couleur donnée en paramètre, faux sinon.
+		:rtype: boolean 
+		"""
+		return self.couleur == couleur
 
 
 	def getMouvementsVersCellule( self ):
+		"""
+		Méthode qui retourne la liste de tous les mouvements allant vers cette cellule.
+
+		:returns: la liste de tous les mouvements allant vers cette cellule.
+		:rtype: liste de :class:'Mouvement'
+		"""
+
 		liste = []
 		for lien in self.getLiens() :
-			liste+= lien.getMouvementVersCellule(self)
+			liste += lien.getMouvementsVersCellule(self)
 		return liste
 
 
 	def _getCoutTotal( self ):
+		"""
+		Calcul le cout totale de la cellule, c'est à dire le cout actuelle ( attaque + defense ),
+		plus toutes les unités des mouvements du joueur propriétaire de cette cellule vers celle-ci,
+		moins toutes les unités de tous les autres unités des mouvements vers cette cellule.
 
-		couleurCellule = self.getCouleurJoueur()
+		:returns: retourne le cout totale de la cellule
+		:rtype: int 
+		"""
+
+		couleurCellule = self.getCouleur()
         
 		coutTotal = self.getCout()
 
 		for mouvement in self.getMouvementsVersCellule() :
 
-			couleurMouvement = mouvement.getCouleurJoueur()
+			couleurMouvement = mouvement.getCouleur()
 
 			if( mouvement.aPourCouleur(couleurCellule) ):
 				coutTotal += mouvement.getNbUnites()
@@ -232,8 +379,21 @@ class Cellule:
 
 
 	def vaEtrePrise( self ):
-		return self._getCoutTotal() < 0
+		"""
+		vérifie si la cellule va être conquise par un autre joueur (elle va changer de propriétaire).
+		Si c'est le cas, elle retourne True, False sinon.
+
+		:returns: retourne vrai si cette cellule va être conquise par un autre joueur, faux sinon
+		:rtype: boolean 
+		"""
+		return self._getCoutTotal() <= 0
 
 
 	def toString(self):
+		"""
+		Représente l'objet sous forme de chaine
+
+		:returns: retourne une chaine de caractère représentant la cellule
+		:rtype: str
+		"""
 		return str( self.getNumero() )
